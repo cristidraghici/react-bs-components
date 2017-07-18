3d1f9ffbdacc939c92254860cd3db4f0fbf222f2
cristi.draghici <cristi@soft-union.ro>
2017-07-18 11:07:33 +0300

    First functional version

--- dist/components/ComboField.js
+++ dist/components/ComboField.js
@@ -22,8 +22,6 @@ var _axios = require('axios');
 
 var _axios2 = _interopRequireDefault(_axios);
 
-require('react-select/dist/react-select.css');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
